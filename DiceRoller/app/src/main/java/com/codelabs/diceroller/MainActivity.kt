1079b33935ac3565ea7e0dd7276ae8efbd42137e
Roberto Hernández Tamayo <roberto.hernandezt@elektra.com.mx>
2020-02-11 10:37:14 -0600

    feat: Actualizaciones de la lección 1

--- DiceRoller/app/src/main/java/com/codelabs/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/codelabs/diceroller/MainActivity.kt
@@ -6,19 +6,23 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
         val rolButton : Button = findViewById(R.id.roll_button)
         rolButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val randomInt = (1..6).random()
-        val diceImage : ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+        diceImage.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -26,6 +30,5 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
     }
 }
